35c97951fc76c89802571cabfc975574d776376a
John Munn <jmunn@getchalk.com>
2021-08-08 14:19:35 -0300

    Adding font smoothing

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -7,6 +7,8 @@ export const GlobalStyle = createGlobalStyle`
   html, body, #root {
     padding: 0;
     background-color: black;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     height: 100vh;
     width: 100vw;
   }
